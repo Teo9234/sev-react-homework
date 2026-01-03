e78c0f5405aea0816e6433fe4d7241c69a637da7
Teo <potheodoros@gmail.com>
2026-01-03 12:35:32 +0200

    feat: added user input on  Question.tsx

--- src/Components/ui/Question.tsx
+++ src/Components/ui/Question.tsx
@@ -1,13 +1,21 @@
-const Question = () => {
-    const answer = "";
+import {useState} from "react";
 
-    return (
-        <div align="center">
+const Question = () => {
+    const [answer, setAnswer] = useState<string>("");
+
+    return  (
+        <div className="flex flex-col items-center text-center">
             <h2 className="text-2xl font-bold mb-4">Perfect Question</h2>
-            <input
-                className="border-3 bg-green-300"
-                type = "text"
-            />
+            <form className="flex items-center mt-8 gap-2">
+                <input
+                    className="border-3 bg-green-300 px-3 py-2"
+                    type="text"
+                    placeholder="I want perfection"
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+
+                />
+            </form>
             <p className="mt-4">Your answer: {answer}</p>
         </div>
     )
