71e582a2f4013c7c9df6b67a93667c88b6bfbecd
Teo <potheodoros@gmail.com>
2026-01-05 18:15:58 +0200

    fix: the Button.tsx now checks both if the window is square and if the answer is perfect

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,35 @@ import Layout from "./Components/layout/Layout.tsx";
 import Button from "./Components/ui/Button.tsx";
 import Question from "./Components/ui/Question.tsx";
 import SizeCounter from "./Components/ui/SizeCounter.tsx";
+import {useEffect, useState} from "react";
+
+const perfectAnswers = ["0", "10", "square", "circle", "zero", "ten"];
 
 function App() {
 
-    // TODO: write the logic
+    const [answerInput, setAnswer] = useState<string>("");
+
+    const [isPerfect, setIsPerfect] = useState(false);
+
+    const handleAnswer = (value:string) => {
+        setAnswer(value);
+    }
+
+    console.log(isPerfect);
+
+    useEffect(() => {
+        setIsPerfect(perfectAnswers.includes(answerInput.toLowerCase()));
+    }, [answerInput]);
+
 
     return (
         <>
             <Layout >
-                <div>
-                    <Question />
+                <div className={"flex flex-col items-center justify-center gap-4"}>
+                    <Question handleAnswer={handleAnswer} answerInput={answerInput}/>
                     <br/>
-                    <Button label={"Not ready"} />
                     <SizeCounter />
+                    <Button label={"Not ready"} isPerfect={isPerfect}/>
                 </div>
             </Layout>
         </>
--- src/Components/ui/Button.tsx
+++ src/Components/ui/Button.tsx
@@ -1,7 +1,11 @@
-import type {ButtonProps} from "../../types.ts";
+import type {ButtonProps, } from "../../types.ts";
+import useWindowSize from "../hooks/useWindowSize.ts";
 
-const Button = ({onClickProp, label}: ButtonProps) => {
+const Button = ({onClickProp, label, isPerfect}: ButtonProps) => {
 
+    // get isSquare from useWindowSize hook
+    const {isSquare} = useWindowSize();
+    console.log(isSquare + " square in Button");
     return (
         <>
             <div className="flex flex-col items-center text-center">
@@ -9,7 +13,7 @@ const Button = ({onClickProp, label}: ButtonProps) => {
                     className={"px-4 py-2 align-middle bg-purple-400 text-white hover:bg-purple-600 hover:cursor-pointer" +
                         " disabled:bg-gray-400 disabled:cursor-not-allowed"}
                     onClick={onClickProp}
-                    disabled={true}
+                    disabled={!isPerfect && !isSquare}
                 >
 
                     {/*Label changes if it is enabled*/}
